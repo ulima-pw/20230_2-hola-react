13e79852d5aaf29de534fef557575e8c382ea391
ProfL3502 <ProfL3502@lab.aca>
2023-01-25 17:24:05 -0500

    Agregando formato de cards

--- src/ListaDiasSemana.jsx
+++ src/ListaDiasSemana.jsx
@@ -1,39 +1,43 @@
 // Componente React
 function ListaDiasSemana(props) {
     if (props.modo === "lista") {
-        return <div>
-            <h3>Dias Semana</h3>
-            <ul className="list-group">
-                {
-                    props.lista.map(function(dia) {
-                        return <li key={ dia }
-                                    className="list-group-item">
-                                    { dia }
-                                </li>
-                    })
-                }
-            </ul>
-        </div>
-    }else if (props.modo === "tabla") {
-        return <div>
-            <h3>Dias Semana</h3>
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Dia</th>
-                    </tr>
-                </thead>
-                <tbody>
+        return <div className="card">
+            <div className="card-body">
+                <h3>Dias Semana</h3>
+                <ul className="list-group">
                     {
                         props.lista.map(function(dia) {
-                            return <tr key={ dia }>
-                                <td>{ dia }</td>
-                            </tr>
+                            return <li key={ dia }
+                                        className="list-group-item">
+                                        { dia }
+                                    </li>
                         })
                     }
-                    
-                </tbody>
-            </table>
+                </ul>
+            </div>
+        </div>
+    }else if (props.modo === "tabla") {
+        return <div className="card">
+            <div className="card-body">
+                <h3>Dias Semana</h3>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Dia</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            props.lista.map(function(dia) {
+                                return <tr key={ dia }>
+                                    <td>{ dia }</td>
+                                </tr>
+                            })
+                        }
+                        
+                    </tbody>
+                </table>
+            </div>
         </div>
     } else {
         return <div>
