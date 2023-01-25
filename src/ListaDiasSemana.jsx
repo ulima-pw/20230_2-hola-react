0ba607ec84da22207b004e3141ce703725ca49fa
ProfL3502 <ProfL3502@lab.aca>
2023-01-25 18:02:47 -0500

    Codigo refactorizado

--- src/ListaDiasSemana.jsx
+++ src/ListaDiasSemana.jsx
@@ -1,5 +1,39 @@
 import { useState } from "react"
 
+function ListaDias(props) {
+    if (props.modo === "tabla") {
+        return <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th>Dia</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    props.lista.map(function(dia) {
+                        return <tr key={ dia }>
+                            <td>{ dia }</td>
+                        </tr>
+                    })
+                } 
+            </tbody>
+        </table>
+    }else if (props.modo === "lista"){
+        return <ul className="list-group">
+            {
+                props.lista.map(function(dia) {
+                    return <li key={ dia }
+                                className="list-group-item">
+                                { dia }
+                            </li>
+                })
+            }
+        </ul>
+    }else {
+        return <div>Modo incorrecto</div>
+    }
+}
+
 // Componente React
 function ListaDiasSemana(props) {
     // Variable de estado
@@ -14,52 +48,15 @@ function ListaDiasSemana(props) {
             setModoVisualizacion("tabla")
         }
     }
+    return <div className="card">
+        <div className="card-body">
+            <h3>Dias Semana <button onClick={ butOnClick } className="btn btn-primary" type="button">Cambiar</button> </h3>
+            <ListaDias modo={ modoVisualizacion } lista={ props.lista }/>
+        </div>
+    </div>   
 
 
-    if (modoVisualizacion === "lista") {
-        return <div className="card">
-            <div className="card-body">
-                <h3>Dias Semana <button onClick={ butOnClick } className="btn btn-primary" type="button">Cambiar</button> </h3>
-                <ul className="list-group">
-                    {
-                        props.lista.map(function(dia) {
-                            return <li key={ dia }
-                                        className="list-group-item">
-                                        { dia }
-                                    </li>
-                        })
-                    }
-                </ul>
-            </div>
-        </div>
-    }else if (modoVisualizacion === "tabla") {
-        return <div className="card">
-            <div className="card-body">
-                <h3>Dias Semana <button onClick={ butOnClick } className="btn btn-primary" type="button">Cambiar</button></h3>
-                <table className="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>Dia</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            props.lista.map(function(dia) {
-                                return <tr key={ dia }>
-                                    <td>{ dia }</td>
-                                </tr>
-                            })
-                        }
-                        
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    } else {
-        return <div>
-            Modo incorrecto
-        </div>
-    }
+    
     
 }
 
