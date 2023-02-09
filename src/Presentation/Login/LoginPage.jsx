c6b24abd05fae35fe4f60a59dc4a251910818c21
ProfL3502 <ProfL3502@lab.aca>
2023-02-08 19:03:15 -0500

    Conectando con el backend el login

--- src/Presentation/Login/LoginPage.jsx
+++ src/Presentation/Login/LoginPage.jsx
@@ -5,12 +5,31 @@ function LoginPage() {
 
     const navigate = useNavigate()
 
-    const onLoginOk = function(
+    const loginHttp = async function(usuario, password) {
+        // 1. Peticion HTTP POST -> /endpoints/login
+        const response = await fetch(
+            "http://localhost:8000/endpoints/login",
+            {
+                method : "POST",
+                body : JSON.stringify(
+                    { 
+                        usuario : usuario, 
+                        password : password 
+                    }
+                )
+            }
+        )
+        const data = await response.json()
+
+        return data.error
+    }
+
+    const onLoginOk = async function(
         usuario, password
     ) {
-        if (usuario === "pw" 
-            && password === "123") {
-
+        const error = await loginHttp(usuario, password)
+        if (error === "") {
+            // Login correcto
             const dataUsuario = {
                 username : usuario,
                 password : password
@@ -18,17 +37,16 @@ function LoginPage() {
 
             // JSON.stringify : convierte objetos js a JSON (string)
             const dataUsuarioJSON = JSON.stringify(dataUsuario)
-            console.log(dataUsuario)
-            console.log(dataUsuarioJSON)
             // Guardado en session storage
             sessionStorage.setItem("DATA_USUARIO", dataUsuarioJSON)
-
 
             navigate("/20230_2-hola-react/main", {
                 state : {
                     username : usuario
                 }
             })
+        }else {
+            console.error(error)
         }
     }
 
